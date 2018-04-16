27d0b54bf375702f2fbd7aacfadd5e00177070df
rodasasfaw <rodasasfaw4@gmail.com>
2018-04-16 17:31:33 -0400

    created models and templates

---
+++ src/main/java/com/userfront/domain/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author RODAS
+ *
+ */
+package com.userfront.domain;
---
+++ src/main/java/com/userfront/service/userserviceimpl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.userfront.service.userserviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.userfront.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+
+}
